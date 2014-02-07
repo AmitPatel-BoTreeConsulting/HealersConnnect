61ff224cca20c641829f973f241be29f68a0dba2
AmitPatel-BoTreeConsulting <amit.patel@botreeconsulting.com>
2014-02-07 21:55:20 +0530

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HealersConnnect::Application.config.secret_token = '93ae2d7fee9d0592e0fe298d10c3e1ad3cc3d1c21fa5379caf8dc5cfd9dd724fe660ea4d13392e7b35fed128659fc2f101f3d8a0c17423713f4359a4d13d54eb'
