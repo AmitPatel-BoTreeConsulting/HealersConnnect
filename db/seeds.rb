3e44458659c7d5ad25677454ff9532f5bddec58b
HardikJoshi-BTC <h4hardikonly@gmail.com>
2014-02-08 09:30:10 +0530

    Created static tables `Role` and `CourceCategory` with Seed #3 master_3

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+def find_or_create_instance class_name, instance_name
+  instance  = class_name.where(name: instance_name)
+  if instance.blank?
+    instance = class_name.create(name: instance_name)
+    puts "#{instance_name} #{class_name} added."
+  else
+    puts "#{instance_name} #{class_name} exists, hence not added it."
+  end
+  instance
+end
+
+# ============== Seed Roles
+roles = [ Role::SUPER_ADMIN_ROLE_TYPE, Role::CENTER_ADMIN_ROLE_TYPE, Role::TEACHER_ROLE_TYPE,
+         Role::HEALER_ROLE_TYPE ]
+
+roles.each { |role_name| find_or_create_instance Role, role_name }
+
+# ============== Seed CourceCategory
+cource_categories = [ 'Healing & Protection', 'Prosperity & Abundance', 'Spirituality' ]
+
+cource_categories.each { |category_name| find_or_create_instance CourceCategory, category_name }
