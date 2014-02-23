a12bdbd2172c30622d884f2a898a8b57b698fd9a
parthivpatel <parthiv.patel@botreeconsulting.com>
2014-02-23 17:30:25 +0530

    Devise Integration for Authentication 1) Sign in page 2) Forgot Password 3) User should not be able to sign up via sign up url

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ gem 'rails', '3.2.16'
 gem 'pg'
 gem 'haml'
 gem 'haml-rails'
+gem "devise", "~> 2.2.4"
+gem "rails_config", "~> 0.3.3"
 
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
@@ -15,4 +17,5 @@ gem 'jquery-rails'
 
 group :development do
   gem 'rails_layout'
+  gem 'letter_opener'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,26 @@ def find_or_create_instance class_name, instance_name
   instance
 end
 
+def find_or_create_user user_attrs
+  email = user_attrs[:email]
+  user = User.find_by_email(email)
+
+  if user.nil?
+    user = User.create(user_attrs)
+    puts "Created user having email #{email}"
+  else
+    puts "User having email #{email} already exists, thus not created"
+  end
+  user
+end
+
+def admin_user_attrs
+  {
+    email: 'admin@healersconnect.com',
+    password: Settings.default_password
+  }
+end
+
 # ============== Seed Roles
 roles = [ Role::SUPER_ADMIN_ROLE_TYPE, Role::CENTER_ADMIN_ROLE_TYPE, Role::TEACHER_ROLE_TYPE,
          Role::HEALER_ROLE_TYPE ]
@@ -19,3 +39,5 @@ roles.each { |role_name| find_or_create_instance Role, role_name }
 cource_categories = [ 'Healing & Protection', 'Prosperity & Abundance', 'Spirituality' ]
 
 cource_categories.each { |category_name| find_or_create_instance CourceCategory, category_name }
+
+admin_user = find_or_create_user(admin_user_attrs)
