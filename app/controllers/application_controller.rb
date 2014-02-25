70527a36e18b9f916cafcb57839d014afa7b99c8
AmitPatel-BoTreeConsulting <amit.patel@botreeconsulting.com>
2014-02-25 08:09:49 +0530

    Authentication via devise for #2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  layout :layout_for_signin
+
+  protected
+  def after_sign_in_path_for(resource)
+    dashboard_path
+  end
+
+  private
+  def layout_for_signin
+    if devise_controller?
+      "auth"
+    else
+      "application"
+    end
+  end
 end
