db45977eed1797de3da08a5ac40aa9fe2d39e357
AmitPatel-BoTreeConsulting <amit.patel@botreeconsulting.com>
2014-03-13 18:20:52 +0530

    Fixing asset precompile issue for fonts

--- config/application.rb
+++ config/application.rb
@@ -64,7 +64,8 @@ module HealersConnnect
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+    
     # https://devcenter.heroku.com/articles/rails-asset-pipeline#failures-in-the-assets-precompile-task
     config.assets.initialize_on_precompile = false
   end
