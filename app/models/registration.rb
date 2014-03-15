89ce31e77ac7ac93254d5f79cf5842b8f827f799
AmitPatel-BoTreeConsulting <amit.patel@botreeconsulting.com>
2014-03-15 08:26:01 +0530

    Fixing Marital status issue

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -3,7 +3,7 @@ class Registration < ActiveRecord::Base
                   :address, :mobile,  :telephone, :email, :location, :long, :lat, :workshop_place, :workshop_dated,
                   :workshop_instructor, :payment_type_id, :fresher, :cheque_no, :bank_name, :cheque_date
 
-  validates_presence_of :first_name, :middle_name, :last_name, :birth_date, :education, :occupation, :gender, :married,
+  validates_presence_of :first_name, :middle_name, :last_name, :birth_date, :education, :occupation,
                         :address, :workshop_place, :workshop_dated, :workshop_instructor
 
   validates :email, presence: true, :format => {:with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/}
