f93345645f4a174bb3ad8755a4191a43fdd6495c
NirajVirani-BTC <niraj.virani@beaverlogic.com>
2014-04-14 21:05:19 +0530

    #31 workshop form fields. wip

--- app/models/workshop.rb
+++ app/models/workshop.rb
@@ -1,9 +1,14 @@
 class Workshop < ActiveRecord::Base
-  attr_accessible :assistant_instructor_id, :center_id, :course_id, :end_date, :fees_after_session, :fees_before_session, :fees_on_rejoining, :fees_on_session, :instructor_id, :start_date
+  attr_accessible :assistant_instructor_id, :center_id,
+    :course_id, :instructor_id, 
+    :end_date, :start_date, 
+    :fees_after_session, :fees_before_session, :fees_on_session,
+    :fees_on_rejoining
 
+  validates_presence_of :center_id, :course_id, :instructor_id 
   belongs_to :course
-  has_one :instructor
-  has_one :assistant_instructor , class: 'Instructor'
+  belongs_to :instructor
+  belongs_to :assistant_instructor, class_name: 'Instructor'
   belongs_to :center
   has_many :workshop_sessions
 
