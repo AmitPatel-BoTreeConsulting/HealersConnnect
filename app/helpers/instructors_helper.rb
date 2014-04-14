7014c94be647cf4cb1b3c8e2155585ec139bc1b8
chetan-beaverlogic <chetan.tomar@beaverlogic.com>
2014-04-14 18:14:43 +0530

    #31 Use 2 (not 1) spaces for indentation. Done

--- app/helpers/instructors_helper.rb
+++ app/helpers/instructors_helper.rb
@@ -1,7 +1,7 @@
 module InstructorsHelper
   def instructor_submit_button
     if @instructor.new_record? 
-    	t('instructor.caption.button.save')
+      t('instructor.caption.button.save')
     else
       t('instructor.caption.button.update')
     end
