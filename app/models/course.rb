cd4af6a30f0d97ba13fa463838ac8d110fd3b295
chetan-beaverlogic <chetan.tomar@beaverlogic.com>
2014-04-14 13:07:37 +0530

    #28  Image issue solved

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,7 +3,15 @@ class Course < ActiveRecord::Base
   attr_accessible :alias, :description, :donation, :eligibility, :name, :review_donation,:course_category_id
 
   attr_accessible :avatar
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "150x150>" }, :default_url => "/images/:style/missing.png"
+  paperclip_options = {
+      styles: {
+          medium: "#{Settings.paperclip.style.medium}>",
+          thumb: "#{Settings.paperclip.style.thumb}>"
+      },
+      :url => Settings.paperclip.image_path
+  }
+
+  has_attached_file :avatar, Paperclip::Attachment.default_options.merge(paperclip_options)
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   validates_presence_of :name, :course_category_id
