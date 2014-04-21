243e938b21e04cba6261789d5544b1df07b49824
chetan-beaverlogic <chetan.tomar@beaverlogic.com>
2014-04-21 12:47:29 +0530

    #42 Hound error resolved

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
+         :recoverable, :rememberable, :trackable,
+         :validatable, authentication_keys: [:login]
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :mobile, :password, :password_confirmation, :remember_me
@@ -64,11 +65,10 @@ class User < ActiveRecord::Base
     end
   end
 
-
   def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
-      where(conditions).where(["lower(mobile) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      where(conditions).where(['lower(mobile) = :value OR lower(email) = :value', { value: login.downcase }]).first
     else
       where(conditions).first
     end
