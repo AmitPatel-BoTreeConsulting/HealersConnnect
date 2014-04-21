2ba67bb94ee14c69202c26a730cc160e509290be
chetan-beaverlogic <chetan.tomar@beaverlogic.com>
2014-04-21 22:53:01 +0530

    schema modified

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140421051505) do
+ActiveRecord::Schema.define(:version => 20140421054515) do
 
   create_table "centers", :force => true do |t|
     t.string   "name"
@@ -189,6 +189,7 @@ ActiveRecord::Schema.define(:version => 20140421051505) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
+    t.string   "mobile"
     t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -200,7 +201,6 @@ ActiveRecord::Schema.define(:version => 20140421051505) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
-    t.string   "mobile",                 :default => "", :null => false
   end
 
   add_index "users", %w(email), name: "index_users_on_email", unique: true
