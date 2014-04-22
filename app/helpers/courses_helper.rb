dad91dc890e50ef9ed322c0b7864b41ae2561b70
chetan-beaverlogic <chetan.tomar@beaverlogic.com>
2014-04-22 15:40:35 +0530

    #53  Current Edit Page doesn't show the eligibility details.  Done

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,2 +1,10 @@
 module CoursesHelper
+  def render_course_eligibility eligibility
+    course_names = []
+    eligibility_split = eligibility.split(',')
+    eligibility_split.each do |course_alias|
+      course_names << Course.find_by_alias(course_alias).name
+    end
+    return course_names.join(', ')
+  end
 end
