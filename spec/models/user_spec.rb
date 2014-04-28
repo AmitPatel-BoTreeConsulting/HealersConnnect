9a12c64fd6b0c58f20e223df15546d6a556008ae
HardikJoshi-BTC <h4hardikonly@gmail.com>
2014-04-28 17:39:01 +0530

    Code refactored in user_spec.rb
    development_59

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,75 +6,11 @@ describe User do
   end
 
   # ========== Validations ==========
-  it 'is invalid without an email' do
-    FactoryGirl.build(:user, email: nil).should_not be_valid
-  end
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password) }
 
-  it 'is invalid if email does not have @' do
-    FactoryGirl.build(:user, email: 'hardik.joshi.com').should_not be_valid
-  end
-
-  it 'is invalid without a password' do
-    FactoryGirl.build(:user, password: nil).should_not be_valid
-  end
-
-  it 'is invalid if password is not 8 characters long' do
-    FactoryGirl.build(:user, password: 1234567).should_not be_valid
-  end
-
-  # ========== Methods ==========
-  describe '#is_foundation_admin?' do
-    it 'returns true for user with role foundation_admin' do
-      user = FactoryGirl.create(:user_with_role,
-        role_name: :foundation_admin)
-      user.is_foundation_admin?.should be_true
-    end
-    it 'returns false for user not having role foundation_admin' do
-      FactoryGirl.create(:user).is_foundation_admin?.should_not be_true
-    end
-  end
-
-  describe '#is_super_admin?' do
-    it 'returns true for user with role super_admin' do
-      user = FactoryGirl.create(:user_with_role,
-        role_name: :super_admin)
-      user.is_super_admin?.should be_true
-    end
-    it 'returns false for user not having role super_admin' do
-      FactoryGirl.create(:user).is_super_admin?.should_not be_true
-    end
-  end
-
-  describe '#is_accountant?' do
-    it 'returns true for user with role accountant' do
-      user = FactoryGirl.create(:user_with_role,
-        role_name: :accountant)
-      user.is_accountant?.should be_true
-    end
-    it 'returns false for user not having role accountant' do
-      FactoryGirl.create(:user).is_accountant?.should_not be_true
-    end
-  end
-
-  describe '#is_center_admin?' do
-    it 'returns true for user with role center_admin' do
-      user = FactoryGirl.create(:user_with_role,
-        role_name: :center_admin)
-      user.is_center_admin?.should be_true
-    end
-    it 'returns false for user not having role center_admin' do
-      FactoryGirl.create(:user).is_center_admin?.should_not be_true
-    end
-  end
-
-  describe '#is_instructor?' do
-    it 'returns true for user with role instructor' do
-      user = FactoryGirl.create(:user_with_role,
-        role_name: :instructor)
-      user.is_instructor?.should be_true
-    end
-    it 'returns false for user not having role instructor' do
-      FactoryGirl.create(:user).is_instructor?.should_not be_true
-    end
-  end
+  it { should allow_value('abc@example.com' ).for(:email) }
+  it { should_not allow_value('abc', 'abc@.').for(:email) }
+  it { should allow_value('12345678' ).for(:password) }
+  it { should_not allow_value('1234567').for(:password) }
 end
