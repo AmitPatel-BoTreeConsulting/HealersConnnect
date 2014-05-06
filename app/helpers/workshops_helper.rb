1c65051a1024819a0c72ce610683384cf1e55692
HardikJoshi-BTC <h4hardikonly@gmail.com>
2014-05-06 11:00:47 +0530

    Code Refactoring Completed 8
    #102
    development_102_1

--- app/helpers/workshops_helper.rb
+++ app/helpers/workshops_helper.rb
@@ -10,9 +10,7 @@ module WorkshopsHelper
   end
 
   def get_options_session_start_end_dropdown
-    options = []
-    options = ['12:00 AM', '01:00 AM', '02:00 AM', '03:00 AM', '04:00 AM', '05:00 AM', '06:00 AM', '07:00 AM', '08:00 AM', '09:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '01:00 PM', '02:00 PM', '03:00 PM', '04:00 PM', '05:00 PM', '06:00 PM', '07:00 PM', '08:00 PM', '09:00 PM', '10:00 PM', '11:00 PM']
-    options
+    ['12:00 AM', '01:00 AM', '02:00 AM', '03:00 AM', '04:00 AM', '05:00 AM', '06:00 AM', '07:00 AM', '08:00 AM', '09:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '01:00 PM', '02:00 PM', '03:00 PM', '04:00 PM', '05:00 PM', '06:00 PM', '07:00 PM', '08:00 PM', '09:00 PM', '10:00 PM', '11:00 PM']
   end
 
   def get_session_start_end_time(session_start_end_time)
