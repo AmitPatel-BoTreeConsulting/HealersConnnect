85e646eea736c04bed54f8bdcad47429a60b7ab5
parthivpatel <parthiv.patel@botreeconsulting.com>
2014-05-07 12:45:23 +0530

    #89 - Change courses dropdown sort by id

--- app/controllers/website/courses_controller.rb
+++ app/controllers/website/courses_controller.rb
@@ -2,13 +2,13 @@ class Website::CoursesController < ApplicationController
   layout 'home'
 
   def index
-    @courses = Course.find_all_by_course_category_id(CourseCategory.first.id).sort_by &:name
+    @courses = Course.find_all_by_course_category_id(CourseCategory.first.id).sort_by &:id
     @course = @courses.first
   end
 
   def category_wise_courses
     respond_to do |format|
-      @courses = Course.find_all_by_course_category_id(params[:id]).sort_by &:name
+      @courses = Course.find_all_by_course_category_id(params[:id]).sort_by &:id
       @course = Course.find(@courses.first)
       format.js {
           render file: 'website/courses/category_wise_courses'
