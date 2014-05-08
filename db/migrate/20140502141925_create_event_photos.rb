d5cbb9d0cc668464e3a5afafa4cf99bea81a4c1b
parthivpatel <parthiv.patel@botreeconsulting.com>
2014-05-08 13:36:05 +0530

    #75 - Change migrations of rename column name and add/remove index for event_photos table

--- db/migrate/20140502141925_create_event_photos.rb
+++ db/migrate/20140502141925_create_event_photos.rb
@@ -1,11 +1,11 @@
 class CreateEventPhotos < ActiveRecord::Migration
   def change
     create_table :event_photos do |t|
-      t.integer :event_schedule_id
-      t.references :event_schedule
+      t.integer :event_id
+      t.references :event
 
       t.timestamps
     end
-    add_index :event_photos, :event_schedule_id
+    add_index :event_photos, :event_id
   end
 end
