07eb3ad5338fed48eb3c98fe2587e722c3ba3bde
HardikJoshi-BTC <h4hardikonly@gmail.com>
2014-05-09 10:23:42 +0530

    delay job integration

--- app/models/certificate_observer.rb
+++ app/models/certificate_observer.rb
@@ -1,8 +1,8 @@
 class CertificateObserver < ActiveRecord::Observer
   def after_create(certificate)
-    HealersConnectMailer.course_complete_notification(
+    HealersConnectMailer.delay.course_complete_notification(
       certificate.workshop,
       certificate.attendee
-    ).deliver
+    )
   end
 end
