fd00bd75ad59960cadcbad77dacb68703a04a9f7
parthivpatel <parthiv.patel@botreeconsulting.com>
2014-06-07 16:59:58 +0530

    #155 - Add jquery boostrap image gallery slider

--- db/schema.rb
+++ db/schema.rb
@@ -127,7 +127,6 @@ ActiveRecord::Schema.define(:version => 20140603031544) do
 
   add_index "donations", ["center_id"], :name => "index_donations_on_center_id"
   add_index "donations", ["donation_type"], :name => "index_donations_on_donation_type"
-  add_index "donations", ["user_id"], :name => "index_donations_on_user_id"
 
   create_table "event_categories", :force => true do |t|
     t.string   "name"
@@ -288,6 +287,7 @@ ActiveRecord::Schema.define(:version => 20140603031544) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => ""
+    t.string   "mobile"
     t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -299,12 +299,11 @@ ActiveRecord::Schema.define(:version => 20140603031544) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
-    t.string   "mobile",                 :default => "", :null => false
     t.string   "member_id"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email"
-  add_index "users", ["member_id"], :name => "index_users_on_member_id", :unique => true
+  add_index "users", ["mobile"], :name => "index_users_on_mobile", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
   create_table "workshop_sessions", :force => true do |t|
