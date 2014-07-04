3432fa2b2f9d4522b4e3f723159212a36b7bbc4c
parthivpatel <parthiv.patel@botreeconsulting.com>
2014-07-04 17:02:33 +0530

    Replace code with precompile

--- config/deploy.rb
+++ config/deploy.rb
@@ -62,5 +62,5 @@ namespace :deploy do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   end
   after "deploy:finalize_update", "deploy:symlink_config"
-  after "deploy", "deploy:assets:precompile"
+  after "deploy", "deploy:asset:generate_assets"
 end
