5ecb20b0f993d465ac5245547b0c3694296aa565
AmitPatel-BoTreeConsulting <amit.patel@botreeconsulting.com>
2014-07-06 17:14:22 +0530

    Fixing asset compile issue on live #171

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ HealersConnnect::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
   config.assets.precompile += %w(canvas_assets.js canvas_assets.css)
-  config.assets.precompile += %w( font-awesome-ie7.min.css pdf/ace.min.css pdf/bootstrap.css pdf/custom.css ace-ie.min.css excanvas.min.js html5shiv.js ckeditor/config.js.coffee)
+  config.assets.precompile += %w( font-awesome-ie7.min.css pdf/ace.min.css pdf/bootstrap.css pdf/custom.css ace-ie.min.css excanvas.min.js html5shiv.js ckeditor/config.js.coffee home/ajax_google.js home/blueimp.js home/bootstrap-image-gallery.min.js)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
