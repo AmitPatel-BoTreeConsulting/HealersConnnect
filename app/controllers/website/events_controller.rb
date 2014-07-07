6f78e3d73ee6b63091b2485093c793b42e11e869
parthivpatel <parthiv.patel@botreeconsulting.com>
2014-07-07 17:57:23 +0530

    #162 - Add events for "Upcomming Events" and "All Events" with AJAX pagination

--- app/controllers/website/events_controller.rb
+++ app/controllers/website/events_controller.rb
@@ -1,6 +1,36 @@
 class Website::EventsController < ApplicationController
   layout 'home'
 
+  def index
+    @page = params[:page] || 1
+    if params[:up_events]
+      @event_schedules = EventSchedule.upcoming_events.page(params[:page]).per(Settings.pagination.per_page).order('created_at ASC')
+    elsif params[:all_events]
+      @events = EventSchedule.page(params[:page]).per(Settings.pagination.per_page).order('created_at DESC')
+    else
+      @event_schedules = EventSchedule.upcoming_events.page(params[:page]).per(Settings.pagination.per_page).order('created_at ASC')
+      @events = EventSchedule.page(params[:page]).per(Settings.pagination.per_page).order('created_at DESC')
+    end
+    respond_to do |format|
+      format.html
+      format.js {
+        render file: 'website/events/index'
+      }
+    end
+  end
+
+  def all_events
+    @page = params[:page] || 1
+    @event_schedules = EventSchedule.upcoming_events.page(params[:page]).per(Settings.pagination.per_page).order('created_at ASC')
+    @events = EventSchedule.page(params[:page]).per(Settings.pagination.per_page).order('created_at DESC')
+    respond_to do |format|
+      format.html
+      format.js {
+        render file: 'website/events/index'
+      }
+    end
+  end
+
   def show
     @event_schedule = EventSchedule.find(params[:id])
   end
